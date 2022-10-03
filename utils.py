67d3968a2d259ce21893d9503bc7669cf3a420ff
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-10-03 01:59:49 +0100

    Added Item, Waste and Bin classes

--- utils.py
+++ utils.py
@@ -14,4 +14,35 @@ def draw(win, images):
         win.blit(img, pos)
 
 
+class Item:
+    def __init__(self, item_path: str=None):
+        self.item_path = item_path
+
+    def load_image(self, factor=False):
+        if factor:
+            return scale_image(pygame.image.load(self.item_path), factor)
+        else:
+            return pygame.image.load(self.item_path)
+
+
+class Bin(Item):
+    def __init__(self, item_path: str=None, position: tuple=None, bin_type: str=None):
+        super().__init__(item_path)
+        self.position = position
+        self.bin_type = bin_type
+
+
+class Waste(Item):
+    def __init__(self, item_path: str=None, waste_type: str=None):
+        super().__init__(item_path)
+        self.waste_type = waste_type
+if __name__ == "__main__":
+    print(scale_image(Item("images/Bin.png").load_image(), 75))
+
+# create an item class
+# create a bin class that inherits from am item class 
+# bin class should have positon and type attribute
+# create waste class
+# waste class should have type attribute 
+
 
